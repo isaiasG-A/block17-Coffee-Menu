c3897576618524216665ed239baa84954d85da85
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-05-02 11:05:29 -0700

    step 4 has been completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const coffeeArray = require("./coffee_data");
 
-          // Step1
-//Method 1: Regular function
+          // Step 3
+//Method 1: Using a Regular function and passing it to forEach()
 function printCoffeeName(coffeeObject) {
   console.log(coffeeObject.name)
 }
@@ -9,15 +9,40 @@ function printCoffeeName(coffeeObject) {
 //coffeeArray.forEach(printCoffeeName);
 
 
-//Method 2: Simplified using arrow function syntax
+//Method 2: Explicit Arrow function
 //forEach() method will execute the arrow function that has a console.log once for each of the objects inside the array.
 
-// coffeeArray.forEach((coffeeObject) => {
-//   console.log(coffeeObject.name)
-// });
+    // coffeeArray.forEach((coffeeObject) => {
+       //   console.log(coffeeObject.name)
+    // });
 
 
-//Method2: An even more simplified version of an arrow function
-coffeeArray.forEach(coffeeObject => console.log(coffeeObject.name));
+//Method 3: Implicit Arrow function
+    coffeeArray.forEach(coffeeObject => console.log(coffeeObject.name));
 
+
+      //Step 4: Drinks that cost $5 and under.
+
+  //Explicit Arrow function
+const filteredDrinks = coffeeArray.filter((coffeeObject) => {
+ let filteredArray = coffeeObject.price === 5;
+
+ filteredArray.map((filteredObject) => {
+    return `${filteredObject.name} costs $${filteredObject.price}`
+ })
+});
+
+console.log(filteredDrinks)
+
+        //Step 5: Array of drinks priced at an even number
+
+ coffeeArray.filter((ArrayObject) => {
+  return (ArrayObject.price % 2) === 0;
+});
+
+
+    //Step 6: Print total
+
+  
+  
 
