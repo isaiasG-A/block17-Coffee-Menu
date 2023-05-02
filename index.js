3c192acbc09fce6b06438c347622d18d95d3ad29
Isaias Garcia <isaias@Isaiass-MacBook-Air.local>
2023-05-02 10:05:37 -0700

    Step 4 has been completed. 3 different ways of printing each coffee name have been implemented

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-const coffeeData = require("./coffee_data");
+const coffeeArray = require("./coffee_data");
+
+          // Step1
+//Method 1: Regular function
+function printCoffeeName(coffeeObject) {
+  console.log(coffeeObject.name)
+}
+
+//coffeeArray.forEach(printCoffeeName);
+
+
+//Method 2: Simplified using arrow function syntax
+//forEach() method will execute the arrow function that has a console.log once for each of the objects inside the array.
+
+// coffeeArray.forEach((coffeeObject) => {
+//   console.log(coffeeObject.name)
+// });
+
+
+//Method2: An even more simplified version of an arrow function
+coffeeArray.forEach(coffeeObject => console.log(coffeeObject.name));
+
+
